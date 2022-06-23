63bc6036eda74f5295028d4eb30f96cb2b7908e0
PisoDev77 <pisodev0427@gmail.com>
2022-06-23 17:26:30 +0900

    setState 2번째

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,13 +4,33 @@ import {useState} from "react";
 
 export default function Counter(){
 
-    
     const [cnt, setCnt] = useState(0);
+    const [show, setShow] = useState(true);
+
+    const operators = ["+","-","*"];
+    const [operator, setOperator] = useState(operators[0]);
 
     return(
         <div>
-            <button onClick={() => {setCnt(cnt + 1)}}>+</button>
-            <h1>Counter: {cnt} !!</h1> 
+            <button onClick={() => {
+                let res = cnt;
+                if(operator === operators[0]){
+                    res += 1;
+                }
+                if(operator === operators[1]){
+                    res -= 1;
+                }
+                if(operator === operators[2]){
+                    res *= 1;
+                }
+                setCnt(res)
+                }}>{operator}</button>
+            <button onClick={() => setShow(!show)}>Show and Hide</button>
+            <button onClick={() => {
+                const idx = Math.floor(Math.random() * operators.length);
+                setOperator(operators[idx]);
+            }}>Change Operator</button>
+            {show && <h1>Counter: {cnt} !!</h1>} 
         </div>
     );
 }
