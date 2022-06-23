bfba1f405756667cbc2a52db8d76f9ca7dd1ca80
PisoDev77 <pisodev0427@gmail.com>
2022-06-23 17:36:15 +0900

    참조 접근시 메모리 주소값으로 접근하기에 상태비교 안돼

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,33 +4,42 @@ import {useState} from "react";
 
 export default function Counter(){
 
-    const [cnt, setCnt] = useState(0);
-    const [show, setShow] = useState(true);
+    // const [cnt, setCnt] = useState(0);
+    // const [show, setShow] = useState(true);
 
     const operators = ["+","-","*"];
-    const [operator, setOperator] = useState(operators[0]);
+    // const [operator, setOperator] = useState(operators[0]);
+    
+    const [info, setInfo] = useState({
+        cnt:0,
+        show:true,
+        operator: operators[0]
+    })
 
     return(
         <div>
             <button onClick={() => {
-                let res = cnt;
-                if(operator === operators[0]){
+                let res = info.cnt;
+                if(info.operator === operators[0]){
                     res += 1;
                 }
-                if(operator === operators[1]){
+                if(info.operator === operators[1]){
                     res -= 1;
                 }
-                if(operator === operators[2]){
+                if(info.operator === operators[2]){
                     res *= 1;
                 }
-                setCnt(res)
-                }}>{operator}</button>
-            <button onClick={() => setShow(!show)}>Show and Hide</button>
+                setInfo({...info, cnt:res})
+            }
+                }>{info.operator}</button>
+            <button onClick={() => {
+                setInfo({...info, show:!info.show})
+            }}>Show and Hide</button>
             <button onClick={() => {
                 const idx = Math.floor(Math.random() * operators.length);
-                setOperator(operators[idx]);
+                setInfo({...info, operator:operators[idx]});
             }}>Change Operator</button>
-            {show && <h1>Counter: {cnt} !!</h1>} 
+            {info.show && <h1>Counter: {info.cnt} !!</h1>} 
         </div>
     );
 }
