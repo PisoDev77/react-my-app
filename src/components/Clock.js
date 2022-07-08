b32b9e2376536178f2e08599c342d67efb954df6
PisoDev77 <pisodev0427@gmail.com>
2022-07-08 17:09:42 +0900

    생명주기 CLASS CLOCK으로 알아봄 /

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,11 +6,35 @@ export default class Clock extends React.Component {
         this.state = {date: new Date()};
     }
 
+    tick() {
+        this.setState({
+            //date 초기화
+            date: new Date(),
+        });
+    }
+
+    //렌더 후 실행
+    componentDidMount() {
+        console.log("YES MOUNT");
+        this.timerID = setInterval(()=>this.tick(), 1000);
+    }
+
+    componentDidUpdate() {
+        console.log("YES UPDATE");
+        console.log(this.state.date);
+    }
+
+    componentWillUnmount() {
+        console.log("YES UNMOUNT");
+        clearInterval(this.timerID);
+    }
+
+
     render(){
         return(
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleString}.</h2>
+                <h2>It is {this.state.date.toLocaleString()}.</h2>
             </div>
         );
     }
