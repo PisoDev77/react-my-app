d9e03fd768323a6921d3eaaf0e22170286693941
PisoDev77 <pisodev0427@gmail.com>
2022-07-08 16:58:31 +0900

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 //import Counter from './components/Counter';
-import Calculator from './components/liftUp/Calculator';
+//import Calculator from './components/liftUp/Calculator';
+import Clock from './components/Clock';
 
 function App() {
 
   return (
     <div className="App">
       {/* <Counter /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      <Clock />
     </div>
   );
 }
 
-export default App;
+export default App;           
